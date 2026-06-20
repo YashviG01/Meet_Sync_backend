c3a5e1d58692108879c41ca46efe909b36130244
YashviG01 <yashvigupta112@gmail.com>
2026-06-21 00:41:10 +0530

    RTC peer connection established

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -31,6 +31,14 @@ const socketHandler = (io) => {
           userName: user.name,
         });
       }
+
+// Notify existing users that a new user joined
+      socket.to(roomId).emit("user-joined", {
+        socketId: socket.id,
+        user,
+      });
+
+
 //participant list
       io.to(roomId).emit(
         "room-users",
@@ -45,40 +53,9 @@ const socketHandler = (io) => {
     });
 
     
-//disconnect
-    socket.on("disconnect", () => {
-      const roomId = socket.roomId;
-
-  if (roomId && roomUsers[roomId]) {
-    roomUsers[roomId] =
-      roomUsers[roomId].filter(
-        (user) =>
-          user.socketId !== socket.id
-      );
-
-    io.to(roomId).emit(
-      "room-users",
-      roomUsers[roomId]
-    );
-  }
 
 
-   const room = io.sockets.adapter.rooms.get(roomId);
-
-const participantCount = room?.size || 0;
-//update participant count when user leaves
-io.to(roomId).emit(
-  "participant-count",
-  participantCount
-);
-  
-
-      console.log(`User disconnected: ${socket.id}`);
-
-    });
-
-
-
+//chat
     socket.on("send-message", ( messageData ) => {
       console.log("backend received", messageData);
       io.to(messageData.roomId).emit("receive-message", 
@@ -102,17 +79,104 @@ io.to(roomId).emit(
 );
 
 
-//offer
-socket.on("offer", () => {});
 
+//webrtc signalling further---
+
+//offer
+  socket.on(
+      "offer",
+      ({ roomId, offer, sender }) => {
+        socket.to(roomId).emit(
+          "offer",
+          {
+            offer,
+            sender,
+          }
+        );
+      }
+    );
 //answer
-socket.on("answer", () => {});
+  socket.on(
+      "answer",
+      ({ roomId, answer, sender }) => {
+        socket.to(roomId).emit(
+          "answer",
+          {
+            answer,
+            sender,
+          }
+        );
+      }
+    );
+
 
 // //ice-candidates
-socket.on("ice-candidate", () => {});
+ socket.on(
+      "ice-candidate",
+      ({
+        roomId,
+        candidate,
+        sender,
+      }) => {
+        socket.to(roomId).emit(
+          "ice-candidate",
+          {
+            candidate,
+            sender,
+          }
+        );
+      }
+    );
+
+
+    //disconnect
+   socket.on("disconnect", () => {
+  const roomId = socket.roomId;
+
+  if (roomId && roomUsers[roomId]) {
+
+    roomUsers[roomId] =
+      roomUsers[roomId].filter(
+        (user) =>
+          user.socketId !== socket.id
+      );
+
+    io.to(roomId).emit(
+      "room-users",
+      roomUsers[roomId]
+    );
+
+    io.to(roomId).emit(
+      "participant-count",
+      roomUsers[roomId].length
+    );
+//applicable for upto 3 participants .will have to switch to target socketId for 3+
+    socket.to(roomId).emit(
+          "user-left",
+          {
+            socketId: socket.id,
+          }
+        );
+
+
+
+    if (roomUsers[roomId].length === 0) {
+      delete roomUsers[roomId];
+    }
+  }
+
+  console.log(
+    `User disconnected: ${socket.id}`
+  );
+});
 
 
   });
+
+  
+
+
+
 };
 
 module.exports = socketHandler;
